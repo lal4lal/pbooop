2f56d9397696db631e08dae32667c23c0ef3c18c
lal4lal <muhammadhilalaja@gmail.com>
2024-12-25 19:17:20 +0700

    register done

--- src/main/java/com/elearning/wave/service/UserService.java
+++ src/main/java/com/elearning/wave/service/UserService.java
@@ -6,7 +6,6 @@ import com.elearning.wave.model.base.Users;
 import com.elearning.wave.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
