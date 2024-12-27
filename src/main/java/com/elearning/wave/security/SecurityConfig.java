a8b45654b8a26a49859229365f19c19ec14ee224
lal4lal <muhammadhilalaja@gmail.com>
2024-12-27 14:09:56 +0700

    enable cors on config

--- src/main/java/com/elearning/wave/security/SecurityConfig.java
+++ src/main/java/com/elearning/wave/security/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthEntryPoint)
